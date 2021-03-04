ec996d671908f7966d954da083d372bc1759d7fe
Caio Santos <caiosantosfreitas@hotmail.com>
2021-03-04 01:49:24 -0300

    refactor age calc debug 2

--- app/src/config/util.js
+++ app/src/config/util.js
@@ -21,7 +21,7 @@ const dateTimeDefault = dt => {
 
 const calculateAge = dt => { 
   const today = new Date()
-  const birthDate = new Date(dt)
+  const birthDate = new Date(dt.split('/').reverse().join('-') + ' 00:00:00')
   let age = today.getFullYear() - birthDate.getFullYear()
   const m = today.getMonth() - birthDate.getMonth()
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
